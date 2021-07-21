036cdd8c56f54336538d9f8f66bebffd1a081f52
William <68653853+will-i-amv@users.noreply.github.com>
2021-07-21 18:03:26 -0400

    Add Knigth subclass that inherits from GameUnit * The subclass also have a new method: acquire_hut, that will implement some of the  logic of play_game

--- Chapter 1/ch01_ex03_new.py
+++ Chapter 1/ch01_ex03_new.py
@@ -142,6 +142,62 @@ class GameUnit:
             print(msg, end=end)
 
 
+class Knight(GameUnit):
+    """ Class that represents the game character 'Knight'
+
+    The player instance in the game is a Knight instance. Other Knight
+    instances are considered as 'friends' of the player and is
+    indicated by the attribute `self.unit_type` .
+    """
+    def __init__(self, name='Sir Foo'):
+        super().__init__(name=name)
+        self.max_hp = 40
+        self.health_meter = self.max_hp
+        self.unit_type = 'friend'
+
+    def info(self):
+        """Print basic information about this character"""
+        print("I  am a Knight!")
+
+    def acquire_hut(self, hut):
+        """Fight the combat (command line) to acquire the hut
+
+        .. todo::   acquire_hut method can be refactored.
+                   Example: Can you use self.enemy instead of calling
+                   hut.occupant every time?
+        """
+        print_bold("Entering hut {}...".format(hut.number), end=' ')
+        is_enemy = (isinstance(hut.occupant, GameUnit) and
+                    hut.occupant.unit_type == 'enemy')
+        continue_attack = 'y'
+        if is_enemy:
+            print_bold("Enemy sighted!")
+            self.show_health(bold=True, end=' ')
+            hut.occupant.show_health(bold=True, end=' ')
+            while continue_attack:
+                continue_attack = input(".......continue attack? (y/n): ")
+                if continue_attack == 'n':
+                    self.run_away()
+                    break
+
+                self.attack(hut.occupant)
+
+                if hut.occupant.health_meter <= 0:
+                    print("")
+                    hut.acquire(self)
+                    break
+                if self.health_meter <= 0:
+                    print("")
+                    break
+        else:
+            if hut.get_occupant_type() == 'unoccupied':
+                print_bold("Hut is unoccupied")
+            else:
+                print_bold("Friend sighted!")
+            hut.acquire(self)
+            self.heal()
+
+
 def reset_health_meter(health_meter):
     """Reset the values of health_meter dict to the original ones"""
     health_meter['player'] = 40
