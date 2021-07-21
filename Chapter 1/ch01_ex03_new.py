8b16c645bb1bdd0b1da4e3b462f76c64be808e4f
William <68653853+will-i-amv@users.noreply.github.com>
2021-07-21 17:30:08 -0400

    Replace the ch01_ex03_new file Use the ch01_ex02 file as a template

--- Chapter 1/ch01_ex03_new.py
+++ Chapter 1/ch01_ex03_new.py
@@ -1,21 +1,18 @@
 from __future__ import print_function
-"""ch01_ex03
+"""ch01_ex02
 
-A text-based game to acquire a hut by defeating the enemy (OOP).
+A text-based game to acquire a hut by defeating the enemy (functional)
 
 This module is compatible with Python 3.5.x. It contains
 supporting code for the book, Learning Python Application Development,
 Packt Publishing.
 
-This is a supporting example code for example 3 of Chapter 1. It is a
-command line program that illustrates use of OOP concepts. The player
-inputs a hut number. If the occupant is an enemy, the player is given an
-option to 'attack'. Player wins if he defeats the enemy.
- Additionally,the player can 'run away' from the combat, get healed
-in friendly hut and then resume the fight.
-
+This is a supporting example code for example 2 of Chapter 1. It is a
+command line program that illustrates use of Python functions.
+The player inputs a hut number. If the occupant is an enemy, the player is
+given an option to 'attack'. Player wins if he defeats the enemy.
 In the aforementioned book this is also referred to as
-"Attack of the Orcs v1.0.0". More details can be found in the relevant
+"Attack of the Orcs v0.0.5". More details can be found in the relevant
 chapter of the book..
 
 RUNNING THE PROGRAM:
@@ -25,7 +22,7 @@ RUNNING THE PROGRAM:
   variable PATH. It will be typically available as 'python' or 'python3'.
 - Here is the command to execute this code from command prompt
 
-        $ python ch01_ex03.py     ( OR $ python3 ch01_ex03.py)
+        $ python ch01_ex02.py     ( OR $ python3 ch01_ex02.py)
 
 - See the README file for more information. Or visit python.org for OS
   specific instructions on executing Python from a command prompt.
@@ -37,284 +34,185 @@ RUNNING THE PROGRAM:
    learn about the code documentation and best practices!
    Feel free to add documentation after reading that chapter.
    Description of the code can be found in the book.
-2. Split the code into smaller modules
-3. Make GameUnit to an abstract base class
-4. See the other TODO comments..things you can try fixing as an exercise!
+
+It is quite simple to make this code compatible even with Python 2.7.9
+as follows. This is not used as this script is intended to be a very simple
+Python illustration.
+# --------------------------------------------------------------------------
+# For backward compatibility with Python 2.7.9
+# Python 2.x, the built-in function 'input' is equivalent to
+# eval(raw_input(prompt)). So we should use just raw_input instead.
+# Also note that we are importing print_function from module __future__
+# for the same reason (see the first line of this file)
+# --------------------------------------------------------------------------
+user_input_function = None
+
+if sys.version_info < (3, 0):
+    user_input_function = raw_input
+else:
+    user_input_function = input
+
+# Then call user_input_function() in places where we call input()
 
 :copyright: 2016, Ninad Sathaye
 
 :license: The MIT License (MIT) . See LICENSE file for further details.
 """
-
 import random
+import textwrap
 import sys
+
 
 if sys.version_info < (3, 0):
     print("This code requires Python 3.x and is tested with version 3.5.x ")
-    print("Looks like you are trying to run this using "
-          "Python version: %d.%d " % (sys.version_info[0],
-                                      sys.version_info[1]))
+    print("Looks like you are trying to run this using " + 
+    "Python version: {}.{} ".format(sys.version_info[0], sys.version_info[1]))
     print("Exiting...")
     sys.exit(1)
 
 
-def weighted_random_selection(obj1, obj2):
-    """Randomly select between two objects based on assigned 'weight'
-
-    .. todo:: How about creating a utility module for common functionality?
-    """
-    weighted_list = 3 * [id(obj1)] + 7 * [id(obj2)]
-    selection = random.choice(weighted_list)
-
-    if selection == id(obj1):
-        return obj1
-
-    return obj2
-
-
 def print_bold(msg, end='\n'):
+    """Print a string in 'bold' font"""
     print("\033[1m" + msg + "\033[0m", end=end)
 
 
-class GameUnit:
-    """A base class for creating various game characters"""
-    def __init__(self, name=''):
-        self.max_hp = 0
-        self.health_meter = 0
-        self.name = name
-        self.enemy = None
-        self.unit_type = None
-
-    def info(self):
-        """Information on the unit (overridden in subclasses)"""
-        pass
-
-    def attack(self, enemy):
-        """The main logic to determine injured unit and amount of injury
-
-        .. todo:: Check if enemy exists!
-        """
-        injured_unit = weighted_random_selection(self, enemy)
-        injury = random.randint(10, 15)
-        injured_unit.health_meter = max(injured_unit.health_meter - injury, 0)
-        print("ATTACK! ", end='')
-        self.show_health(end='  ')
-        enemy.show_health(end='  ')
-
-    def heal(self, heal_by=2, full_healing=True):
-        """Heal the unit replenishing all the hit points"""
-        if self.health_meter == self.max_hp:
-            return
-
-        if full_healing:
-            self.health_meter = self.max_hp
-        else:
-            # TODO: Do you see a bug here? it can exceed max hit points!
-            self.health_meter += heal_by
-
-        print_bold("You are HEALED!", end=' ')
-        self.show_health(bold=True)
-
-    def reset_health_meter(self):
-        """Reset the `health_meter` (assign default hit points)"""
-        self.health_meter = self.max_hp
-
-    def show_health(self, bold=False, end='\n'):
-        """Show the remaining hit points of the player and the enemy"""
-        # TODO: what if there is no enemy?
-        msg = "Health: %s: %d" % (self.name, self.health_meter)
-
-        if bold:
-            print_bold(msg, end=end)
-        else:
-            print(msg, end=end)
+def print_dotted_line(width=72):
+    """Print a dotted (rather 'dashed') line"""
+    print('-'*width)
 
 
-class Knight(GameUnit):
-    """ Class that represents the game character 'Knight'
+def show_theme_message(width=72):
+    """Print the game theme in the terminal window"""
+    print_dotted_line()
+    print_bold("Attack of The Orcs v0.0.5:")
+    msg = (
+        "The war between humans and their arch enemies, Orcs, was in the "
+        "offing. Sir Foo, one of the brave knights guarding the southern "
+        "plains began a long journey towards the east through an unknown "
+        "dense forest. On his way, he spotted a small isolated settlement."
+        " Tired and hoping to replenish his food stock, he decided to take"
+        " a detour. As he approached the village, he saw five huts. There "
+        "was no one to be seen around. Hesitantly, he  decided to enter..")
 
-    The player instance in the game is a Knight instance. Other Knight
-    instances are considered as 'friends' of the player and is
-    indicated by the attribute `self.unit_type` .
-    """
-    def __init__(self, name='Sir Foo'):
-        super().__init__(name=name)
-        self.max_hp = 40
-        self.health_meter = self.max_hp
-        self.unit_type = 'friend'
-
-    def info(self):
-        """Print basic information about this character"""
-        print("I  am a Knight!")
-
-    def acquire_hut(self, hut):
-        """Fight the combat (command line) to acquire the hut
-
-        .. todo::   acquire_hut method can be refactored.
-                   Example: Can you use self.enemy instead of calling
-                   hut.occupant every time?
-        """
-        print_bold("Entering hut %d..." % hut.number, end=' ')
-        is_enemy = (isinstance(hut.occupant, GameUnit) and
-                    hut.occupant.unit_type == 'enemy')
-        continue_attack = 'y'
-        if is_enemy:
-            print_bold("Enemy sighted!")
-            self.show_health(bold=True, end=' ')
-            hut.occupant.show_health(bold=True, end=' ')
-            while continue_attack:
-                continue_attack = input(".......continue attack? (y/n): ")
-                if continue_attack == 'n':
-                    self.run_away()
-                    break
-
-                self.attack(hut.occupant)
-
-                if hut.occupant.health_meter <= 0:
-                    print("")
-                    hut.acquire(self)
-                    break
-                if self.health_meter <= 0:
-                    print("")
-                    break
-        else:
-            if hut.get_occupant_type() == 'unoccupied':
-                print_bold("Hut is unoccupied")
-            else:
-                print_bold("Friend sighted!")
-            hut.acquire(self)
-            self.heal()
-
-    def run_away(self):
-        """Abandon the battle.
-
-        .. seealso:: `self.acquire_hut`
-        """
-        print_bold("RUNNING AWAY...")
-        self.enemy = None
+    print(textwrap.fill(msg, width=width))
 
 
-class OrcRider(GameUnit):
-    """Class that represents the game character Orc Rider"""
-    def __init__(self, name=''):
-        super().__init__(name=name)
-        self.max_hp = 30
-        self.health_meter = self.max_hp
-        self.unit_type = 'enemy'
-        self.hut_number = 0
-
-    def info(self):
-        """Print basic information about this character"""
-        print("Grrrr..I am an Orc Wolf Rider. Don't mess with me.")
+def reset_health_meter(health_meter):
+    """Reset the values of health_meter dict to the original ones"""
+    health_meter['player'] = 40
+    health_meter['enemy'] = 30
 
 
-class Hut:
-    """Class to create hut object(s) in the game Attack of the Orcs"""
-    def __init__(self, number, occupant):
-        self.occupant = occupant
-        self.number = number
-        self.is_acquired = False
-
-    def acquire(self, new_occupant):
-        """Update the occupant of this hut"""
-        self.occupant = new_occupant
-        self.is_acquired = True
-        print_bold("GOOD JOB! Hut %d acquired" % self.number)
-
-    def get_occupant_type(self):
-        """Return a string giving info on the hut occupant"""
-        if self.is_acquired:
-            occupant_type = 'ACQUIRED'
-        elif self.occupant is None:
-            occupant_type = 'unoccupied'
-        else:
-            occupant_type = self.occupant.unit_type
-
-        return occupant_type
+def show_game_mission():
+    """Print the game mission in the terminal window"""
+    print_bold("Mission:")
+    print("\tChoose a hut where Sir Foo can rest...")
+    print_bold("TIP:")
+    print("Be careful as there are enemies lurking around!")
+    print_dotted_line()
 
 
-class AttackOfTheOrcs:
-    """Main class to play Attack of The Orcs game"""
-    def __init__(self):
-        self.huts = []
-        self.player = None
+def occupy_huts():
+    """Randomly populate the `huts` list with occupants"""
+    huts = []
+    occupants = ['enemy', 'friend', 'unoccupied']
+    while len(huts) < 5:
+        computer_choice = random.choice(occupants)
+        huts.append(computer_choice)
+    return huts
 
-    def get_occupants(self):
-        """Return a list of occupant types for all huts.
 
-        .. todo::
+def process_user_choice():
+    """Accepts the hut number from the user"""
+    msg = "\033[1m" + "Choose a hut number to enter (1-5): " + "\033[0m"
+    user_choice = input("\n" + msg)
+    idx = int(user_choice)
+    return idx
 
-             Prone to bugs if self.huts is not populated.
-             Chapter 2 talks about catching exceptions
-        """
-        return [x.get_occupant_type() for x in self.huts]
 
-    def show_game_mission(self):
-        """Print the game mission in the console"""
-        print_bold("Mission:")
-        print("  1. Fight with the enemy.")
-        print("  2. Bring all the huts in the village under your control")
-        print("---------------------------------------------------------\n")
+def reveal_occupants(huts, idx):
+    """Print the occupants of the hut"""
+    print("Revealing the occupants...")
+    msg = ""
+    for i in range(len(huts)):
+        occupant_info = "<{}:{}>".format(i+1, huts[i])
+        if i + 1 == idx:
+            occupant_info = "\033[1m" + occupant_info + "\033[0m"
+        msg += occupant_info + " "
+    print("\t" + msg)
+    print_dotted_line()
 
-    def _process_user_choice(self):
-        """Process the user input for choice of hut to enter"""
-        verifying_choice = True
-        idx = 0
-        print("Current occupants: %s" % self.get_occupants())
-        while verifying_choice:
-            user_choice = input("Choose a hut number to enter (1-5): ")
-            idx = int(user_choice)
-            if self.huts[idx-1].is_acquired:
-                print("You have already acquired this hut. Try again."
-                      "<INFO: You can NOT get healed in already acquired hut.>")
-            else:
-                verifying_choice = False
 
-        return idx
+def show_health(health_meter, bold=False):
+    """Show the remaining hit points of the player and the enemy"""
+    msg = "Health: Sir Foo: {}, Enemy: {}".format(
+        health_meter['player'], health_meter['enemy'])
+    if bold:
+        print_bold(msg)
+    else:
+        print(msg)
 
-    def _occupy_huts(self):
-        """Randomly occupy the huts with one of: friend, enemy or 'None'"""
-        for i in range(5):
-            choice_lst = ['enemy', 'friend', None]
-            computer_choice = random.choice(choice_lst)
-            if computer_choice == 'enemy':
-                name = 'enemy-' + str(i+1)
-                self.huts.append(Hut(i+1, OrcRider(name)))
-            elif computer_choice == 'friend':
-                name = 'knight-' + str(i+1)
-                self.huts.append(Hut(i+1, Knight(name)))
-            else:
-                self.huts.append(Hut(i+1, computer_choice))
 
-    def play(self):
-        """Workhorse method to play the game.
+def attack(health_meter):
+    """The main logic to determine injured unit and amount of injury"""
+    hit_list = 4 * ['player'] + 6 * ['enemy']
+    injured_unit = random.choice(hit_list)
+    hit_points = health_meter[injured_unit]
+    injury = random.randint(10, 15)
+    health_meter[injured_unit] = max(hit_points - injury, 0)
+    print("ATTACK! ", end='')
+    show_health(health_meter)
 
-        Controls the high level logic to play the game. This is called from
-        the main program to begin the game execution.
-        """
-        self.player = Knight()
-        self._occupy_huts()
-        acquired_hut_counter = 0
 
-        self.show_game_mission()
-        self.player.show_health(bold=True)
+def play_game(health_meter, huts, idx):
+    """The main control function for playing the game"""
+    if huts[idx - 1] != 'enemy':
+        print_bold("Congratulations! YOU WIN!!!")
+    else:
+        print_bold('ENEMY SIGHTED! ', end='')
+        show_health(health_meter, bold=True)
+        continue_attack = True
 
-        while acquired_hut_counter < 5:
-            idx = self._process_user_choice()
-            self.player.acquire_hut(self.huts[idx-1])
+        # Loop that actually runs the combat if user wants to attack
+        while continue_attack:
+            continue_attack = input(".......continue attack? (y/n): ")
+            if continue_attack == 'n':
+                print_bold("RUNNING AWAY with following health status...")
+                show_health(health_meter, bold=True)
+                print_bold("GAME OVER!")
+                break
 
-            if self.player.health_meter <= 0:
+            attack(health_meter)
+
+            # Check if either one of the opponents is defeated
+            if health_meter['enemy'] <= 0:
+                print_bold("GOOD JOB! Enemy defeated! YOU WIN!!!")
+                break
+
+            if health_meter['player'] <= 0:
                 print_bold("YOU LOSE  :(  Better luck next time")
                 break
 
-            if self.huts[idx-1].is_acquired:
-                acquired_hut_counter += 1
 
-        if acquired_hut_counter == 5:
-            print_bold("Congratulations! YOU WIN!!!")
+def run_application():
+    keep_playing = 'y'
+    health_meter = {}
+    
+    show_theme_message()
+    reset_health_meter(health_meter)
+    show_game_mission()
+
+    # The main while loop. Keep playing depending on the user input.
+    while keep_playing == 'y':
+        reset_health_meter(health_meter)
+        huts = occupy_huts()
+        idx = process_user_choice()
+        reveal_occupants(huts, idx)
+
+        play_game(health_meter, huts, idx)
+
+        keep_playing = input("Play again? Yes(y)/No(n):")
 
 
 if __name__ == '__main__':
-    game = AttackOfTheOrcs()
-    game.play()
-
+    run_application()
