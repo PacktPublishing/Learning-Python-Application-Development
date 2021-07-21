75558595e64482743fae9d30c6ab2ddbfc67ccf7
William <68653853+will-i-amv@users.noreply.github.com>
2021-07-21 17:54:10 -0400

    Add GameUnit class to store player's info * The following functions will be moved as methods of the class: attack reset_health_meter show_health * The class will also have these new methods: info heal

--- Chapter 1/ch01_ex03_new.py
+++ Chapter 1/ch01_ex03_new.py
@@ -88,6 +88,60 @@ def print_bold(msg, end='\n'):
     print("\033[1m" + msg + "\033[0m", end=end)
 
 
+class GameUnit:
+    """A base class for creating various game characters"""
+    def __init__(self, name=''):
+        self.max_hp = 0
+        self.health_meter = 0
+        self.name = name
+        self.enemy = None
+        self.unit_type = None
+
+    def info(self):
+        """Information on the unit (overridden in subclasses)"""
+        pass
+
+    def attack(self, enemy):
+        """The main logic to determine injured unit and amount of injury
+
+        .. todo:: Check if enemy exists!
+        """
+        injured_unit = weighted_random_selection(self, enemy)
+        injury = random.randint(10, 15)
+        injured_unit.health_meter = max(injured_unit.health_meter - injury, 0)
+        print("ATTACK! ", end='')
+        self.show_health(end='  ')
+        enemy.show_health(end='  ')
+
+    def heal(self, heal_by=2, full_healing=True):
+        """Heal the unit replenishing all the hit points"""
+        if self.health_meter == self.max_hp:
+            return
+
+        if full_healing:
+            self.health_meter = self.max_hp
+        else:
+            # TODO: Do you see a bug here? it can exceed max hit points!
+            self.health_meter += heal_by
+
+        print_bold("You are HEALED!", end=' ')
+        self.show_health(bold=True)
+
+    def reset_health_meter(self):
+        """Reset the `health_meter` (assign default hit points)"""
+        self.health_meter = self.max_hp
+
+    def show_health(self, bold=False, end='\n'):
+        """Show the remaining hit points of the player and the enemy"""
+        # TODO: what if there is no enemy?
+        msg = "Health: {}: {}".format(self.name, self.health_meter)
+
+        if bold:
+            print_bold(msg, end=end)
+        else:
+            print(msg, end=end)
+
+
 def reset_health_meter(health_meter):
     """Reset the values of health_meter dict to the original ones"""
     health_meter['player'] = 40
