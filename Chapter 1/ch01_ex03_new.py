b22d323020a52a68970ad1137dc344d88de6412b
William <68653853+will-i-amv@users.noreply.github.com>
2021-07-21 18:09:56 -0400

    Add Hut class to store hut's info

--- Chapter 1/ch01_ex03_new.py
+++ Chapter 1/ch01_ex03_new.py
@@ -212,6 +212,31 @@ class OrcRider(GameUnit):
         print("Grrrr..I am an Orc Wolf Rider. Don't mess with me.")
 
 
+class Hut:
+    """Class to create hut object(s) in the game Attack of the Orcs"""
+    def __init__(self, number, occupant):
+        self.occupant = occupant
+        self.number = number
+        self.is_acquired = False
+
+    def acquire(self, new_occupant):
+        """Update the occupant of this hut"""
+        self.occupant = new_occupant
+        self.is_acquired = True
+        print_bold("GOOD JOB! Hut %d acquired" % self.number)
+
+    def get_occupant_type(self):
+        """Return a string giving info on the hut occupant"""
+        if self.is_acquired:
+            occupant_type = 'ACQUIRED'
+        elif self.occupant is None:
+            occupant_type = 'unoccupied'
+        else:
+            occupant_type = self.occupant.unit_type
+
+        return occupant_type
+
+
 def reset_health_meter(health_meter):
     """Reset the values of health_meter dict to the original ones"""
     health_meter['player'] = 40
